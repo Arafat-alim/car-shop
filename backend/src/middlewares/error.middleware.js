bb801d3c7665c93719b46f3cdd5aa69983d8474d
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-07-01 23:53:05 +0530

    🙎🏻‍♂️`Middleware`: Error middleware added and implemented.

---
+++ backend/src/middlewares/error.middleware.js
@@ -0,0 +1,31 @@
+import { ApiError } from '../utils/ApiError.js';
+
+const errorHandler = (err, req, res, next) => {
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Internal Server Error';
+
+  if (!(err instanceof ApiError)) {
+    //! development error
+    if (process.env.NODE_ENV === 'development') {
+      console.error(err);
+    }
+
+    //! production error
+    if (process.env.NODE_ENV === 'production') {
+      statusCode = 500;
+      message = 'Internal Server Error';
+    }
+  }
+
+  const response = {
+    success: false,
+    statusCode,
+    message,
+    errors: err.errors || [],
+    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined,
+  };
+
+  return res.status(statusCode).json(response);
+};
+
+export { errorHandler };
