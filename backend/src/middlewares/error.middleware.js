cd9a215f9ce65c76bdd2dd073194dbb498fd0997
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-07-02 00:41:41 +0530

    🙎🏻‍♂️`Middleware`: Error middleware updated.

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -1,5 +1,6 @@
 import { ApiError } from '../utils/ApiError.js';
 
+// eslint-disable-next-line no-unused-vars
 const errorHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || 'Internal Server Error';
