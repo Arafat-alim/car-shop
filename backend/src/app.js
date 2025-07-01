646ac6dd54c67ebbd5bef473db99f3e87317b4e4
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-07-01 17:18:08 +0530

    📃 `Logger`: implemented, morgan and winston

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,38 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 const app = express();
+
+import logger from './utils/logger.js';
+import morgan from 'morgan';
+
+const morganFormat = ':method :url :status :response-time ms';
 
 //! common middlewares
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-  })
+  }),
 );
 
-app.use(express.json({ limit: "16kb" })); // allowing request of json type data with limit 16kb (you can update this value)
-app.use(express.urlencoded({ extended: true, limit: "16kb" })); // handling the url route request coming from the client
-app.use(express.static("public")); // any images, css file, etc
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(' ')[0],
+          url: message.split(' ')[1],
+          status: message.split(' ')[2],
+          responseTime: message.split(' ')[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  }),
+);
+
+app.use(express.json({ limit: '16kb' })); // allowing request of json type data with limit 16kb (you can update this value)
+app.use(express.urlencoded({ extended: true, limit: '16kb' })); // handling the url route request coming from the client
+app.use(express.static('public')); // any images, css file, etc
 
 export { app };
