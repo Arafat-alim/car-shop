e663ad2780cff7e440a17c826bba521cea9c3991
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-07-02 03:16:41 +0530

    ✅ `static file`: Now apis is available for testing as a web page.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,15 @@ import express from 'express';
 import cors from 'cors';
 const app = express();
 
+import path from 'path';
+
 import logger from './utils/logger.js';
 import morgan from 'morgan';
+import { fileURLToPath } from 'url';
 
 const morganFormat = ':method :url :status :response-time ms';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //! common middlewares
 app.use(
@@ -33,6 +38,12 @@ app.use(
 
 app.use(express.json({ limit: '16kb' })); // allowing request of json type data with limit 16kb (you can update this value)
 app.use(express.urlencoded({ extended: true, limit: '16kb' })); // handling the url route request coming from the client
-app.use(express.static('public')); // any images, css file, etc
+// app.use(express.static('public')); // any images, css file, etc
+app.use('/public', express.static(path.join(__dirname, '../public')));
+
+// Route to serve the API docs HTML file
+app.get('/api-docs', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/car-shop-apis.html'));
+});
 
 export { app };
