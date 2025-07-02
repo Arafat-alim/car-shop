ef1945026f2ad8d80d0a957acaf65a32f468f844
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-07-02 17:53:16 +0530

    ☑️ `Auth Routes`: Added refreshtoken and updated user model.

--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['user', 'admin'], default: 'user' },
+    refreshToken: {
+      type: String,
+      select: false,
+    },
   },
   { timestamps: true },
 );
